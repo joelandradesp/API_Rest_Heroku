a052ab0904ca6d2d8c03c9b60e35bac94abbe5de
joelandradesp <joelguarulhos@gmail.com>
2023-01-30 22:59:19 -0300

    Feito Get, Get by id sem usar Optional, atualizado README.md

--- apirest/src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
+++ apirest/src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.produtos.apirest.models.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	Produto findById(long id);
 
 }
